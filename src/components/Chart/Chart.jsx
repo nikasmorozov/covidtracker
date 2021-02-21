f28f8a87e2f7bee1e2c578c8bf1fc70ef61c3f2f
Nikas <solarisocean@gmail.com>
2021-02-21 23:07:44 +0200

    done

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -38,7 +38,6 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
       ) : null
 
   );
-  console.log(confirmed, recovered, deaths);
 
   const barChart = (
     confirmed
